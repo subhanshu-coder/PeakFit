3b042333e5d804490f5a8dde105fb279af21e5f7
Subhanshu pal <subhanshupal7@gmail.com>
2026-08-07 23:44:57 +0530

    update all

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,9 @@
+/// <reference types="vite/client" />
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import './index.css'
 import App from './App.tsx'
+import './index.css' // Import the Tailwind CSS file
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
